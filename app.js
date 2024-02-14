ec74ad9611fa12abf793daa3a539513665ad7cbe
kdeepthi123 <kondavitideepthi@gmail.com>
2024-02-14 11:33:38 -0600

    js code changes

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ document.addEventListener("swiped-up",function(e){
 function display(){
    let elem = 0;
    for(let row=0;row<4;row++) {
-    
+    for(let col=0;col<4;col++){
+        if(board[row][col]===0){
+            boardContainer.children[elem].style.color="";
+            boardContainer.children[elem].innerText="";
+            boardContainer.children[elem].style.backgroundColor="";
+            elem++;
+        }
+        else{
+            if(board[row][col]>=128){
+                boardContainer.children[elem].style.color="white";
+            }
+            boardContainer.children[elem].innerText=board[row][col];
+            boardContainer.children[elem].style.backgroundColor=changeColor(row,col);
+elem++;
+        }
+    }
    }
 }
+
