acd10b161f57158cace39b6bec0362c2917188b1
kdeepthi123 <kondavitideepthi@gmail.com>
2024-02-14 11:25:21 -0600

    new files

---
+++ app.js
@@ -0,0 +1,17 @@
+const board=[
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+]
+const boardContainer=document.querySelector(".board");
+document.addEventListener("swiped-up",function(e){
+    console.log(e.target);
+})
+
+function display(){
+   let elem = 0;
+   for(let row=0;row<4;row++) {
+    
+   }
+}
